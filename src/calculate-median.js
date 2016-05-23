ce0e81f136ac7752fcd72d68e8eac5a97c10bddc
Ruyu You <727809166@qq.com>
2016-05-23 20:41:51 +0800

    finish calculate-median.js

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,5 +1,29 @@
 function calculate_median(arr) {
-  //请勿改动函数名
+    //请勿改动函数名
+    var evenNumber = selectAllEvenNumber(arr);
+    var medianNumber = buildMedianNumber(evenNumber);
+    return medianNumber;
+}
+
+function selectAllEvenNumber(array) {
+    var evenNumber = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            evenNumber.push(array[i]);
+        }
+    }
+    return evenNumber;
+}
+
+function buildMedianNumber(evenNumber) {
+    var medianNumber;
+    if (evenNumber.length % 2 === 0) {
+        var sumMedianNumber = evenNumber[evenNumber.length / 2] + evenNumber[evenNumber.length / 2 - 1];
+        medianNumber = sumMedianNumber / 2;
+    } else {
+        medianNumber = evenNumber[Math.floor(evenNumber.length / 2)];
+    }
+    return medianNumber;
 }
 
 module.exports = calculate_median;
